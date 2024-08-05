34a779f5cca3dd5c2ce7f12391f73cc607f617e2
DanyPechene <dany.pechene@gers.com>
2024-08-05 14:04:39 -0500

    login - avance 12

--- panel-admin/includes/sesion/login.php
+++ panel-admin/includes/sesion/login.php
@@ -1,48 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login | Mi Plaza Autoservicio</title>
+    <title>Login | Administrador</title>
 
-    <link rel="stylesheet" href="../../css/bootstrap.min.css">
     <link rel="stylesheet" href="../../css/style.css">
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-    <script src="https://www.google.com/recaptcha/enterprise.js?render=6Lcb9nknAAAAAJ8aouGGquPrx5H9qw-c7XFAABQK"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a81368914c.js"></script>
 </head>
-
 <body>
+    <img class="wave" src="../img/ima1.png">
     <div class="container">
-        <div id="login"></div>
-        <div class="row justify-content-center align-items-center min-vh-100">
-            <div id="login-box" class="col-12 col-md-8 col-lg-6">
-                <h2 class="text-center">GERS S.A.S</h2>
-                <p class="text-center">Ingrese su usuario y contraseña para iniciar sesión</p>
-                <div id="alert">
+        <div class="img">
+        </div>
+        <div class="login-content">
+        <form class="row g-3 needs-validation" novalidate id="loginForm" name="loginData" action="validar.php" action="validar_admin.php" method="POST">
+                <img src="../img/profile.png">
+                <h2 class="title">Bienvenido</h2>
+                <div class="input-div one">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" id="usuario" name="usuario" class="form-control" placeholder="Usuario" required>
+                    </div>
                 </div>
-                <form class="row g-3 needs-validation" novalidate id="loginForm" name="loginData" action="validar.php" action="validar_admin.php" method="POST">
+                <div class="input-div pass">
+                    <div class="i"> 
+                        <i class="fas fa-lock"></i>
+                    </div>
                     <div class="form-group">
-                        <label for="usuario" class="form-label">Usuario</label>
-                        <input type="text" id="usuario" name="usuario" class="form-control" required>
+                        <input type="password" id="password" name="password" class="form-control" placeholder="Contraseña" required>
                     </div>
-
-                    <div class="form-group">
-                        <label for="password" class="form-label">Contraseña</label>
-                        <input type="password" id="password" name="password" class="form-control" required>
-                    </div>
-                    <div class="text-center">
-                        <button data-sitekey="6Lcb9nknAAAAAGF7kYkpuYU_9nfpA1j8rM1rq8bd" type="submit" class="btn btn-primary">Ingresar</button>
-                    </div>
-                </form>
-                <br>
-
-                
-            </div>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary">Ingresar</button>
+                </div>
+            </form>
         </div>
     </div>
     <script src="../../js/login.js"></script>
 </body>
-
 </html>
